da0fe1eb659cabb6bad35e1e36670595c8fc666f
Justin Beckwith <beckwith@google.com>
2018-10-14 17:26:11 -0700

    feat: add npm event hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+
 /**
- * Triggered from a message on a Cloud Pub/Sub topic.
- *
- * @param {!Object} event Event payload.
- * @param {!Object} context Metadata for the event.
+ * @interface javascriptEvent
+ * @property {string} repo
+ * @property {string} file
+ * @property {string} language
+ * @property {string} event
  */
+
 exports.javascriptProcessor = (event, context) => {
   const message = event.data;
-  console.log(Buffer.from(message, 'base64').toString());
+  const data = JSON.parse(Buffer.from(message, 'base64').toString());
+  console.log(data);
 };
+
+exports.npmEvent = (event, context) => {
+  const message = event.data;
+  const data = JSON.parse(Buffer.from(message, 'base64').toString());
+  console.log(data);
+}
