2cc8bbb62ffb94004a15cae2a2b4ebdbdc59d057
Justin Beckwith <beckwith@google.com>
2018-10-20 20:18:43 -0700

    test: create basic test structure

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
 const Firestore = require('@google-cloud/firestore');
+const axios = require('axios');
+
 const db = new Firestore();
-const modules = db.collection('npm-modules');
 
 /**
  * @interface javascriptEvent
- * @property {string} repo
- * @property {string} file
- * @property {string} language
- * @property {string} event
+ * @property {string} repo - owner/repoName
+ * @property {string} file - package.json
+ * @property {string} language - JavaScript
+ * @property {string} event - 🤷‍♂️
  */
 
-exports.javascriptProcessor = (event, context) => {
+/**
+  * When a package.json file has been added or changed,
+  * do the following stuff:
+  * - Fetch the file from GitHub
+  * - Iterate over dependencies and devDependencies
+  * - Save the collection of paths that reference the dep
+  * - Trigger the check workflow for that dep/repo combo
+  */
+exports.javascriptProcessor = async (event, context) => {
   const message = event.data;
   const data = JSON.parse(Buffer.from(message, 'base64').toString());
-  console.log(data);
+  const docPath = `${data.repo}/${data.file}`;
+  const url = `https://api.github.com/repos/${data.repo}/contents/${data.file}`;
+  const res = await axios.get(url);
+  const packageJson = res.data;
+  ['dependencies', 'devDependencies'].forEach(deps => {
+    if (packageJson[deps]) {
+      Object.keys(packageJson[deps]).forEach(async dep => {
+        console.log(`Updating dep: ${dep}`);
+        await db.collection('npm-modules')
+          .doc(dep)
+          .collection('packageFiles')
+          .set(docPath, {
+            path: docPath,
+            version: packageJson.dependencies[dep]
+          });
+      });
+    }
+  });
 };
 
 /**
@@ -26,5 +52,9 @@ exports.npmEvent = async (event, context) => {
   const message = event.data;
   const data = JSON.parse(Buffer.from(message, 'base64').toString());
   console.log(data);
-  await modules.doc(data.name).set(data);
-}
+  // query all repos that use this package
+  const snapshot = await db.collection('npm-modules').doc(data.name).collection('packageFiles');
+  snapshot.forEach(doc => {
+    console.log(JSON.stringify(doc));
+  });
+};
--- test/test.js
+++ test/test.js
@@ -1,6 +1,47 @@
 const assert = require('assert');
-const javascriptProcessor = require('../src/index.js');
+const nock = require('nock');
+const proxyquire = require('proxyquire');
+const pkg = require('./fixtures/fakePackage.json');
+const evt = require('./fixtures/fakeJSEvent.json');
 
-it('should work', async () => {
-  assert(javascriptProcessor);
+class MockFirestoreCollection {
+  constructor () {
+    this.values = new Map();
+  }
+  doc (name) {
+    return new MockFirestoreDoc();
+  }
+  async set (name, value) {
+    this.values.set(name, value);
+  }
+}
+
+class MockFirestoreDoc {
+  constructor () {
+    this.collections = new Map();
+  }
+  collection (name) {
+    if (!this.collections.has(name)) {
+      this.collections.set(name, new MockFirestoreCollection());
+    }
+    return this.collections.get(name);
+  }
+}
+
+const service = proxyquire('../src/index.js', {
+  '@google-cloud/firestore': MockFirestoreDoc
+});
+
+describe('javascript-processor', () => {
+  it('should update dependencies', async () => {
+    const basePath = 'https://api.github.com';
+    const ghPath = `/repos/${evt.repo}/contents/${evt.file}`;
+    const scope = nock(basePath).get(ghPath).reply(200, pkg);
+    const event = {
+      data: Buffer.from(JSON.stringify(evt), 'utf8')
+    };
+    await service.javascriptProcessor(event);
+    scope.done();
+    assert(true);
+  });
 });
