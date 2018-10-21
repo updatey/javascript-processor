cf7596f4b56dbe0e5ffe610b6acbc97cc0dc39e0
Justin Beckwith <beckwith@google.com>
2018-10-21 12:14:41 -0700

    test: add basic test for updateIfNeeded

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ const proxyquire = require('proxyquire');
 const pkg = require('./fixtures/fakePackage.json');
 const evt = require('./fixtures/fakeJSEvent.json');
 const npmEvent = require('./fixtures/fakeNpmEvent.json');
+const registryResponse = require('./fixtures/fakeRegistryResponse.json');
 
 nock.disableNetConnect();
 
@@ -14,21 +15,21 @@ class MockFirestoreCollection {
   }
   doc (name) {
     if (!this.data.has(name)) {
-      this.data.set(name, new MockFirestoreDoc())
+      this.data.set(name, new MockFirestoreDoc());
     }
     return this.data.get(name);
   }
-  async get() {
+  async get () {
     return [...this.data.keys()].map(key => {
       return {
         id: key,
         data: () => {
           return this.data.get(key);
         }
-      }
+      };
     });
   }
-  async set(key, value) {
+  async set (key, value) {
     this.data.set(key, value);
   }
 }
@@ -57,52 +58,53 @@ afterEach(() => {
   sandbox.restore();
 });
 
-describe('javascript-processor', () => {
-  it('should store dependencies', async () => {
-    const basePath = 'https://api.github.com';
-    const ghPath = `/repos/${evt.repo}/contents/${evt.file}`;
-    const scope = nock(basePath).get(ghPath).reply(200, pkg);
-    const stub = sandbox.stub(service, 'updateIfNeeded').resolves();
-    const event = {
-      data: Buffer.from(JSON.stringify(evt), 'utf8')
-    };
-    await service.javascriptProcessor(event);
+it('should store dependencies', async () => {
+  const basePath = 'https://api.github.com';
+  const ghPath = `/repos/${evt.repo}/contents/${evt.file}`;
+  const scope = nock(basePath).get(ghPath).reply(200, pkg);
+  const stub = sandbox.stub(service, 'updateIfNeeded').resolves();
+  const event = {
+    data: Buffer.from(JSON.stringify(evt), 'utf8')
+  };
+  await service.javascriptProcessor(event);
 
-    // ensure the API call to GitHub actually happened
-    scope.done();
+  // ensure the API call to GitHub actually happened
+  scope.done();
 
-    // ensure the `updateIfNeeded` method was invoked
-    assert(stub.called);
+  // ensure the `updateIfNeeded` method was invoked
+  assert(stub.called);
 
-    // ensure all 7 modules in the fixture response are found
-    const npmModules = await service.db.collection('npm-modules').get();
-    assert.strictEqual(npmModules.length, 7);
+  // ensure all 7 modules in the fixture response are found
+  const npmModules = await service.db.collection('npm-modules').get();
+  assert.strictEqual(npmModules.length, 7);
 
-    // ensure the cloudcats package.json is the only one
-    const codecov = service.db.collection('npm-modules').doc('codecov');
-    const packageFiles = await codecov.collection('packageFiles').get();
-    assert.strictEqual(packageFiles.length, 1);
+  // ensure the cloudcats package.json is the only one
+  const codecov = service.db.collection('npm-modules').doc('codecov');
+  const packageFiles = await codecov.collection('packageFiles').get();
+  assert.strictEqual(packageFiles.length, 1);
 
-    // ensure it finds the right value
-    const packageFile = packageFiles[0];
-    assert.strictEqual(packageFile.id, 'JustinBeckwith/cloudcats/package.json');
-    assert.strictEqual(packageFile.data().version, '^3.1.0');
-  });
+  // ensure it finds the right value
+  const packageFile = packageFiles[0];
+  assert.strictEqual(packageFile.id, 'JustinBeckwith/cloudcats/package.json');
+  assert.strictEqual(packageFile.data().version, '^3.1.0');
 });
 
-describe('npm-events', () => {
+it('should find all relevant packages', async () => {
+  const event = {
+    data: Buffer.from(JSON.stringify(npmEvent), 'utf8')
+  };
+  const stub = sandbox.stub(service, 'updateIfNeeded').resolves();
+  await service.npmEvent(event);
+  assert.strictEqual(stub.called, true);
 
-  it('should find all relevant packages', async () => {
-    const event = {
-      data: Buffer.from(JSON.stringify(npmEvent), 'utf8')
-    };
-    const stub = sandbox.stub(service, 'updateIfNeeded').resolves();
-    await service.npmEvent(event);
-    assert.strictEqual(stub.called, true);
-
-    // TODO: go beyond basic tests to verify the times called
-  });
-
+  // TODO: go beyond basic tests to verify the times called
 });
 
-
+it('should update if needed', async () => {
+  const fakePackageFile = '/does/not/exist/package.json';
+  const fakeDep = 'retry-axios';
+  const fakeVersion = '^0.2.0';
+  const scope = nock('https://registry.npmjs.org').get('/retry-axios').reply(200, registryResponse);
+  await service.updateIfNeeded(fakePackageFile, fakeDep, fakeVersion);
+  scope.done();
+});
